a6a217cc0e9dc7da10240ee8ccdd070cf7ab9b30
Jean Castro <jean.castro.42@gmail.com>
2026-03-05 22:47:23 +0000

    Adicionando tabuleiro e Navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,22 +2,45 @@
 
 int main() {
 
-char tabuleiro[10][10];
-char linha = '0';
-char coluna = 'A';
+char tabuleiro[10][10]; //DECLARANDO VARIAVEIS 
+char navio = '3';
+char agua = '0';
+int linha[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 
 printf("===BATALHA NAVAL===\n");
 printf("  A B C D E F G H I J\n");
-for (int i = 0; i < 10; i++) {
-    printf("%d ", i);
-    for (int j = 0; j < 10; j++) {  
-        tabuleiro[i][j] = '0'; 
-        printf("%c ", tabuleiro[i][j]);
-    }
-    printf("\n");
-}   
 
+    
+for (int i = 0; i < 10; i++){
+    for (int j = 0; j < 10; j++)
+    {
+        tabuleiro[i][j] = agua; //TRANFORMANDO O TABOLEIRO EM ÁGUA
+    }
+}  
+    //ADICIONANDO NAVIOS
+    
+    tabuleiro[9][4] = navio;
+    tabuleiro[9][5] = navio; // NAVIO 1
+    tabuleiro[9][6] = navio;
+
+
+    tabuleiro[0][0] = navio;
+    tabuleiro[0][1] = navio; // NAVIO 2
+    tabuleiro[0][2] = navio;
+
+    tabuleiro[2][5] = navio;
+    tabuleiro[3][5] = navio; // NAVIO 3
+    tabuleiro[4][5] = navio;
+
+for (int i = 0; i < 10; i++){
+        printf("%d ", linha[i]);
+    for (int j = 0; j < 10; j++)
+    {
+        printf("%c ", tabuleiro[i][j]); // MOSTRAR O TABULEIRO COMPLETO
+    }
+        printf("\n");
+}
 
     return 0;
 }
